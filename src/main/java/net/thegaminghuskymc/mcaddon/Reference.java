30c54d3f583835185b643561ac964ecbf3c7af59
BuildTools <unconfigured@null.spigotmc.org>
2018-03-26 09:06:18 -0400

    Cleaned

--- src/main/java/net/thegaminghuskymc/mcaddon/Reference.java
+++ src/main/java/net/thegaminghuskymc/mcaddon/Reference.java
@@ -1,9 +1,8 @@
 package net.thegaminghuskymc.mcaddon;
 
-public class Reference {
-
+public class Reference
+{
     public static final String MOD_ID = "mc_addon";
     public static final String NAME = "Husky's Minecraft Addon";
     public static final String VERSION = "0.0.1";
-
 }
