3ae3d366ae993457963cf417482983558dd16031
BuildTools <unconfigured@null.spigotmc.org>
2018-03-29 13:37:09 -0400

    Entity Scorpion

---
+++ src/main/java/net/thegaminghuskymc/mcaddon/entity/render/RenderScorp.java
@@ -0,0 +1,30 @@
+package net.thegaminghuskymc.mcaddon.entity.render;
+
+import net.minecraft.client.renderer.entity.RenderLiving;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.util.ResourceLocation;
+import net.thegaminghuskymc.mcaddon.Reference;
+import net.thegaminghuskymc.mcaddon.entity.EntityScorp;
+import net.thegaminghuskymc.mcaddon.entity.render.model.ModelScorp;
+
+public class RenderScorp extends RenderLiving<EntityScorp>
+{
+    public static final ResourceLocation SCORP_TEXTURE = new ResourceLocation(Reference.MOD_ID + ":textures/entity/scorp.png");
+
+    public  RenderScorp(RenderManager manager)
+    {
+        super(manager, new ModelScorp(), 0.5F);
+    }
+
+    @Override
+    protected ResourceLocation getEntityTexture(EntityScorp entity)
+    {
+        return  SCORP_TEXTURE;
+    }
+
+    @Override
+    protected void applyRotations(EntityScorp entityLiving, float p_77043_2_, float rotationYaw, float partialTicks)
+    {
+        super.applyRotations(entityLiving, p_77043_2_, rotationYaw, partialTicks);
+    }
+}
