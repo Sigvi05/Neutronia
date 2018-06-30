50c3542fe699e0d8188385b5ccbe5192d8fafc75
sindrefag <sindrefagerheim53@gmail.com>
2018-06-30 23:31:20 +0200

    last code for today

--- src/main/java/net/hdt/neutronia/util/Reference.java
+++ src/main/java/net/hdt/neutronia/util/Reference.java
@@ -4,11 +4,10 @@ public class Reference {
 
     public static final String MOD_ID = "neutronia";
     public static final String NAME = "Neutronia";
-    public static final String VERSION = "0.0.5";
+    public static final String VERSION = "0.1.1";
     public static final String DEPENDENCIES = "required-before:hl2;";
 
     public static final String CLIENT_PROXY = "net.hdt.neutronia.proxy.ClientProxy";
     public static final String SERVER_PROXY = "net.hdt.neutronia.proxy.CommonProxy";
-    public static final String NETWORK_CHANNEL_NAME = MOD_ID;
 
 }
