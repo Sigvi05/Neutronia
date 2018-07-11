d177fef85797567c957c6a5ca536aa306aa253c7
TheGamingHuskyMC <sindrefagerheim53@gmail.com>
2018-07-11 12:36:54 +0200

    new beta \o/

--- src/main/java/net/hdt/neutronia/util/Reference.java
+++ src/main/java/net/hdt/neutronia/util/Reference.java
@@ -4,10 +4,9 @@ public class Reference {
 
     public static final String MOD_ID = "neutronia";
     public static final String NAME = "Neutronia";
-    public static final String VERSION = "0.1.3";
+    public static final String VERSION = "0.2.0";
     public static final String DEPENDENCIES = "required-before:hl2;";
     public static final String UPDATE_JSON = "https://gist.githubusercontent.com/sindrefag/be19331edf8ae73628450304918911cc/raw/76bfdb4aa14b5ac631cfa1861548351247a2350e/update_json_neutronia.json";
-
     public static final String CLIENT_PROXY = "net.hdt.neutronia.proxy.ClientProxy";
     public static final String SERVER_PROXY = "net.hdt.neutronia.proxy.CommonProxy";
 
