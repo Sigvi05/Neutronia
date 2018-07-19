31e59bdffc4529ae60453554ad0bf9bc753f9521
Dbrown55 <dbrown55.gaming@gmail.com>
2018-07-19 14:10:05 +0200

    Fixed scaffolding blocks so you can climb them like ladders
    (Why can't you have ladders that are full blocks in Minecraft?)
    
    Also made so I won't push my "setup.bat" file

--- src/main/java/net/hdt/neutronia/blocks/overworld/BlockScaffoldingBlock.java
+++ src/main/java/net/hdt/neutronia/blocks/overworld/BlockScaffoldingBlock.java
@@ -1,5 +1,11 @@
 package net.hdt.neutronia.blocks.overworld;
 
+import static net.hdt.neutronia.util.Reference.MOD_ID;
+
+import java.util.Objects;
+
+import javax.annotation.Nullable;
+
 import net.hdt.huskylib2.blocks.BlockMod;
 import net.hdt.neutronia.init.NBlocks;
 import net.minecraft.block.SoundType;
@@ -9,24 +15,23 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.stats.StatList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import javax.annotation.Nullable;
-import java.util.Objects;
-
-import static net.hdt.neutronia.util.Reference.MOD_ID;
-
 public class BlockScaffoldingBlock extends BlockMod {
 
+	private static AxisAlignedBB AABB = new AxisAlignedBB(1.0/16.0, 0, 1.0/16.0, 15.0/16.0, 1, 15.0/16.0);
+	
     public BlockScaffoldingBlock() {
         super(Material.WOOD, MOD_ID, "scaffolding_block");
         this.setCreativeTab(CreativeTabs.DECORATIONS);
@@ -34,13 +39,28 @@ public class BlockScaffoldingBlock extends BlockMod {
         setHarvestLevel("axe", 0);
         this.setSoundType(SoundType.WOOD);
     }
-
+    
     public boolean isOpaqueCube(IBlockState state) {
         return false;
     }
 
     public boolean isFullCube(IBlockState state) {
         return false;
+    }
+    
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+    	return AABB;
+    }
+    
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+    	return AABB;
+    }
+    
+    @Override
+    public AxisAlignedBB getSelectedBoundingBox(IBlockState state, World worldIn, BlockPos pos) {
+    	return AABB;
     }
 
     public void harvestBlock(World worldIn, EntityPlayer player, BlockPos pos, IBlockState state, @Nullable TileEntity te, ItemStack stack) {
@@ -59,34 +79,7 @@ public class BlockScaffoldingBlock extends BlockMod {
 
     @Override
     public boolean isLadder(IBlockState state, IBlockAccess world, BlockPos pos, EntityLivingBase entity) {
-        return true;
-    }
-
-    @Override
-    public void onEntityCollision(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
-        super.onEntityCollision(worldIn, pos, state, entityIn);
-        if (entityIn instanceof EntityLivingBase && !((EntityLivingBase) entityIn).isOnLadder() && isLadder(state, worldIn, pos, (EntityLivingBase) entityIn)) {
-            float f5 = 0.15F;
-            if (entityIn.motionX < -f5)
-                entityIn.motionX = -f5;
-            if (entityIn.motionX > f5)
-                entityIn.motionX = f5;
-            if (entityIn.motionZ < -f5)
-                entityIn.motionZ = -f5;
-            if (entityIn.motionZ > f5)
-                entityIn.motionZ = f5;
-
-            entityIn.fallDistance = 0.0F;
-            if (entityIn.motionY < -0.15D)
-                entityIn.motionY = -0.15D;
-
-            if (entityIn.motionY < 0 && entityIn instanceof EntityPlayer && entityIn.isSneaking()) {
-                entityIn.motionY = 0;
-                return;
-            }
-            if (entityIn.collidedHorizontally)
-                entityIn.motionY = .2;
-        }
+    	return true;
     }
 
 }
