beb514d069809ca4604db30daf568df93fb5c89b
Dbrown55 <dbrown55.gaming@gmail.com>
2018-07-22 21:08:01 +0200

    Worked a bit on the module system
    Looks like you can "manually" parse a JsonElement to something else

---
+++ src/main/java/net/hdt/neutronia/base/module_new/Feature.java
@@ -0,0 +1,11 @@
+package net.hdt.neutronia.base.module_new;
+
+import net.minecraft.util.ResourceLocation;
+
+public class Feature {
+
+	private String name, description;
+	private ResourceLocation icon_texture;
+	private boolean enabledByDefault, enabled;
+	
+}
--- src/main/java/net/hdt/neutronia/base/module_new/Module.java
+++ src/main/java/net/hdt/neutronia/base/module_new/Module.java
@@ -1,12 +1,71 @@
 package net.hdt.neutronia.base.module_new;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class Module implements Comparable<Module> {
 
+	private static Gson GSON = new GsonBuilder().registerTypeAdapter(ItemStack.class, new JsonDeserializer<ItemStack>() {
+
+		@Override
+		public ItemStack deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+			Item item = null;
+			int count = 1; // Would it even be used here ?
+			int metadata = 0;
+			
+			if(json instanceof JsonObject) {
+				JsonObject jsonObj = (JsonObject) json;
+				
+				// Try to get item
+				try {
+					item = ForgeRegistries.ITEMS.getValue(new ResourceLocation(jsonObj.get("id").getAsString()));
+				} catch(Exception e) { }
+				
+				// Try to get count (uncomment if could be useful)
+				try {
+					count = jsonObj.get("count").getAsInt();
+				} catch(Exception e) { }
+				
+				// Try to get metadata
+				try {
+					metadata = jsonObj.get("metadata").getAsInt();
+				} catch(Exception e) { }
+			}
+			
+			return item == null ? null : new ItemStack(item, count, metadata);
+		}
+		
+	}).registerTypeAdapter(ResourceLocation.class, new JsonDeserializer<ResourceLocation>() {
+
+		@Override
+		public ResourceLocation deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+			try {
+				return new ResourceLocation(json.getAsString());
+			} catch(Exception e) {
+				return null;
+			}
+		}
+				
+	}).create();
+	
     private String name, description;
-    private ItemStack iconStack;
+    private ItemStack icon_stack;
+    private ArrayList<Feature> features;
+    private boolean enabledByDefault, enabled;
 
     public String getName() {
         if(name != null) {
@@ -21,8 +80,8 @@ public class Module implements Comparable<Module> {
     }
 
     public ItemStack getIconStack() {
-        if(iconStack != null)
-            return iconStack;
+        if(icon_stack != null)
+            return icon_stack;
         else
             return new ItemStack(Blocks.BARRIER);
     }
